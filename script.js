b15085988078346efbe3e2d6aa60c94394c0d31d
Kondurisrinivas <srinukonduri7703@gmail.com>
2023-07-11 18:24:10 +0530

    modified

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-console.log('hello world')
-console.log('hello world')
+document.title='DOM Manipulation';
+var headerTitle=document.getElementById('header-title');
+headerTitle.textContent='hello';
+headerTitle.innerText='Hey';
+headerTitle.innerHTML='<h3> Heading </h3>';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
 
-console.log('hello world')
-
-console.log('hello world')
-
-
-console.log('srinivas')
+var tit=document.querySelector('.title');
+tit.style.fontWeight='bold';
+tit.style.color='green';
