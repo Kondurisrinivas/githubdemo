e43a4bec8989678aef74d8b9c5c5a340ef233c07
Kondurisrinivas <srinukonduri7703@gmail.com>
2023-07-11 20:03:44 +0530

    getelementbytagname

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ var lis=document.getElementsByClassName('list-group-item');
 for(var i=0;i<lis.length;i++){
     lis[i].style.fontWeight='bold';
 }
+
+var tit=document.getElementsByTagName('li');
+tit[2].style.backgroundColor='green'
+
+var lis=document.getElementsByTagName('li');
+for(var i=0;i<lis.length;i++){
+    lis[i].style.fontWeight='bold';
+}
