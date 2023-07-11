29a427738e5bfce3e1d46dd04d85f37c6e842cd7
Kondurisrinivas <srinukonduri7703@gmail.com>
2023-07-11 19:35:26 +0530

    GetElementByClass

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ header.style.borderBottom='solid 3px #000';
 
 
 
-var tit=document.querySelectorAll('.list-group-item');
+var tit=document.getElementsByClassName('list-group-item');
 tit[2].style.backgroundColor='green'
 
-var lis=document.querySelectorAll('.list-group-item');
-console.log(lis)
+var lis=document.getElementsByClassName('list-group-item');
 for(var i=0;i<lis.length;i++){
     lis[i].style.fontWeight='bold';
 }
